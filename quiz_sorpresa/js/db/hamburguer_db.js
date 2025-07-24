294a1119878d0dd745672571b880e100a237e572
Alexi_Dg <alexisdurangomez588@gmail.com>
2025-07-23 22:03:32 -0500

    feat: :saprkles: added findbrg function

--- quiz_sorpresa/js/db/hamburguer_db.js
+++ quiz_sorpresa/js/db/hamburguer_db.js
@@ -25,14 +25,17 @@ export const ingredients = [
 
 export const categories = [
     {
+        "id": 1,
         "nombre": "Clásica",
         "descripcion": "Hamburguesas clásicas y sabrosas"
     },
     {
+        "id": 2,
         "nombre": "Vegetariana",
         "descripcion": "Hamburguesas sin carne, perfectas para vegetarianos"
     },
     {
+        "id": 3,
         "nombre": "Gourmet",
         "descripcion": "Hamburguesas gourmet con ingredientes premium"
     }
@@ -40,6 +43,7 @@ export const categories = [
 
 export const burguers = [
     {
+        "id": 1,
         "nombre": "Clásica",
         "categoria": "Clásica",
         "ingredientes": ["Pan", "Carne de res", "Queso cheddar", "Lechuga", "Tomate", "Cebolla", "Mayonesa", "Ketchup"],
@@ -47,6 +51,7 @@ export const burguers = [
         "chef": "ChefA"
     },
     {
+        "id": 2,
         "nombre": "Vegetariana",
         "categoria": "Vegetariana",
         "ingredientes": ["Pan integral", "Hamburguesa de lentejas", "Queso suizo", "Espinacas", "Cebolla morada", "Aguacate", "Mayonesa vegana"],
@@ -54,6 +59,7 @@ export const burguers = [
         "chef": "ChefB"
     },
     {
+        "id": 3,
         "nombre": "Doble Carne",
         "categoria": "Gourmet",
         "ingredientes": ["Pan de sésamo", "Doble carne de res", "Queso cheddar", "Bacon", "Lechuga", "Cebolla caramelizada", "Salsa BBQ"],
@@ -64,14 +70,17 @@ export const burguers = [
 
 export const chefs = [
     {
+        "id": 1,
         "nombre": "ChefA",
         "especialidad": "Carnes"
     },
     {
+        "id": 2,
         "nombre": "ChefB",
         "especialidad": "Cocina Vegetariana"
     },
     {
+        "id": 3,
         "nombre": "ChefC",
         "especialidad": "Gourmet"
     }
--- quiz_sorpresa/js/services/chef_services.js
+++ quiz_sorpresa/js/services/chef_services.js
@@ -1,7 +1,39 @@
 // Chefs Services File
+import { chefs } from "../db/hamburguer_db.js";
+import createId from "../helpers/create_id.js";
 
 export const findChef = () => {
-    // CREATE NEW CHEF
+    // SEARCH CHEF
+    let searchBy = prompt(`
+        Por favor, ingrese 'n' para filtrar por nombre
+        o 'i' para filtrar por ID: 
+        `);
+    
+        switch (searchBy.toLocaleLowerCase()) {
+            case 'n':
+                let nombre  = prompt('Ingrese el nombre: ');
+                let exist = chefs.filter( chef => chef.nombre.toLocaleLowerCase() == nombre.toLocaleLowerCase());
+                if (!exist) {
+                    alert(`El ingrediente ${nombre} no existe.`);
+                } else {
+                    alert(`Ingrediente: ${JSON.stringify(exist, null, 2)}`)
+                }
+                break;
+    
+            case 'i':
+                let ID  = prompt('Ingrese el ID: ');
+                let exist_id = chefs.find( chef => chef.id == parseInt(ID));
+                if (!exist_id) {
+                    alert(`El ingrediente ${nombre} no existe.`);
+                } else {
+                    alert(`Ingrediente: ${JSON.stringify(exist_id, null, 2)}`)
+                }
+                break;
+        
+            default:
+                alert(`Por favor ingresa una opción valida.`);
+                break;
+        };
 };
 
 export const createChef = () => {
@@ -9,14 +41,14 @@ export const createChef = () => {
 };
 
 export const readChef = () => {
-    // CREATE NEW CHEF
+    // READE CHEFS
 };
 
 export const updateChef = () => {
-    // CREATE NEW CHEF
+    // UPDATE CHEF
 };
 
 export const deleteChef = () => {
-    // CREATE NEW CHEF
+    // DELETE CHEF
 };
 // Developed by Alexi Duran C.c: 1.067.031.983
