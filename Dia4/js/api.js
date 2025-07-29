56c0d68eaa5cee7293738c42b90b42a698c56519
Alexi_Dg <alexisdurangomez588@gmail.com>
2025-07-28 20:19:27 -0500

    feat: :fire: traying added pagination

--- Dia4/js/api.js
+++ Dia4/js/api.js
@@ -1,34 +1,61 @@
-const xhr = new XMLHttpRequest();
-const url = "https://rickandmortyapi.com/api/character";
+const btnBack = document.getElementById('back');
+const btnNext = document.getElementById('next');
+let paginaActual = 1;
 
-xhr.open("GET", url, true);
-
-xhr.onreadystatechange = function () {
-  if (xhr.readyState === 4) {
-    if (xhr.status === 200) {
-      const respuesta = JSON.parse(xhr.responseText);
-      const personajes = respuesta.results;
-
-      const contenedor = document.getElementById("personajes");
-
-      for (let i = 0; i < personajes.length; i++) {
-        const personaje = personajes[i];
-
-        const div = document.createElement("div");
-        div.innerHTML = `
-          <h3>${personaje.name}</h3>
-          <img src="${personaje.image}" alt="${personaje.name}" width="150">
-          <p>Especie: ${personaje.species}</p>
-          <p>Estado: ${personaje.status}</p>
-        `;
-        contenedor.appendChild(div);
+function loadCharacters(pagina) {
+    const xhr = new XMLHttpRequest();
+    const url = `https://rickandmortyapi.com/api/character?page=${pagina}`;
+    
+    xhr.open("GET", url, true);
+    
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          const respuesta = JSON.parse(xhr.responseText);
+          const personajes = respuesta.results;
+          const info = respuesta.info;
+    
+          const contenedor = document.getElementById("personajes");
+          contenedor.innerHTML = "";
+    
+          for (let i = 0; i < personajes.length; i++) {
+            const personaje = personajes[i];
+    
+            const div = document.createElement("div");
+            div.innerHTML = `
+              <h3>${personaje.name}</h3>
+              <img src="${personaje.image}" alt="${personaje.name}" width="150">
+              <p>Especie: ${personaje.species}</p>
+              <p>Estado: ${personaje.status}</p>
+              <p>Genero: ${personaje.gender}</p>
+              <p>Origen: ${personaje.origin.name}</p>
+            `;
+            contenedor.appendChild(div);
+          }
+          // Controlar botones
+            document.getElementById("back").disabled = !info.prev;
+            document.getElementById("next").disabled = !info.next;
+        } else {
+          console.error("Error al cargar la API", xhr.status);
+        }
       }
-    } else {
-      console.error("Error al cargar la API", xhr.status);
-    }
+    };
+    
+    xhr.send();
+}
+
+// Botones
+document.getElementById("back").addEventListener("click", function () {
+  if (paginaActual > 1) {
+    paginaActual--;
+    cargarPersonajes(paginaActual);
   }
-};
+});
 
-xhr.send();
+document.getElementById("next").addEventListener("click", function () {
+  paginaActual++;
+  cargarPersonajes(paginaActual);
+});
 
-
+// Cargar primera página al iniciar
+loadCharacters(paginaActual);
