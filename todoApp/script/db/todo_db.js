30118875efb1bd2ac130426992ac08d0ee38e7c7
Alexi_Dg <alexisdurangomez588@gmail.com>
2025-08-06 14:32:08 -0500

    feat: :rocket: added db file

---
+++ todoApp/script/db/todo_db.js
@@ -0,0 +1,8 @@
+const db = [
+    {
+        "id": 1,
+        "name": "Lavar platos",
+        "message": "Limpiar primero el baño y luego la sala.",
+        "category": "Household chores"
+    }
+];
