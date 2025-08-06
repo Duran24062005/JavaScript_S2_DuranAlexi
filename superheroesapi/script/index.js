6fcaec4b877cd6a8c6fe05ea8886606db890ddd4
Alexi_Dg <alexisdurangomez588@gmail.com>
2025-08-05 19:45:33 -0500

    fix: :sparkles: fixing functionalities

--- superheroesapi/script/index.js
+++ superheroesapi/script/index.js
@@ -5,10 +5,10 @@ function searchSuperHeroes() {
     const name = document.getElementById('inputName').value;
     const container = document.getElementById('containerHeroes');
     let actualHero = 'a-bomb';
-    let hero = name ? name.trim().toLowerCase() : actualHero;
     if (name) {
         actualHero = "";
     }
+    let hero = name ? name.trim().toLowerCase() : actualHero;
 
     const xhr = new XMLHttpRequest();
     let url = `https://superheroapi.com/api.php/e2cc7e19e3c8c79da98153ad21f672f8/search/${hero}`;
@@ -18,16 +18,18 @@ function searchSuperHeroes() {
             if (xhr.readyState === 4 && xhr.status === 200) {
                 const data = JSON.parse(xhr.responseText);
                 console.log(data.results[0])
+                container.innerHTML = "";
                 data.results.forEach(e => {
                     container.innerHTML += `
-                    <div class="card col" style="width: 18rem;">
-                        <img src="${e.image.url}" class="card-img-top" alt="Foto del super heroe ${e.name}">
-                        <div class="card-body">
-                            <h5 class="card-title">${e.name}</h5>
-                            <h5 class="text-bold">${e.biography['full-name']}</h5>
-                            <p class="card-text">${e.work.occupation}</p>
-                            <p class="card-text">${e.work.base}</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <div class="col-sm-6 col-md-4 col-lg-3">
+                        <div class="card h-100">
+                            <img src="${e.image.url}" class="card-img-top" alt="Foto del super heroe ${e.name}">
+                            <div class="card-body">
+                                <h5 class="card-title">${e.name}</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">${e.biography['full-name']}</h6>
+                                <p class="card-text">${e.work.occupation}</p>
+                                <p class="card-text"><small>${e.work.base}</small></p>
+                            </div>
                         </div>
                     </div>
                     `;
