4ca00f3099d6ddee01bd3b672079a1faac24e021
Alexi_Dg <alexisdurangomez588@gmail.com>
2025-08-06 10:31:33 -0500

    feat: :rocket: added todo app structure

---
+++ todoApp/script/script.js
@@ -0,0 +1,11 @@
+function deleteItem(id) {
+    alert(`Item ${id} deleted successfully`);
+}
+
+function update(id) {
+    alert(`Item ${id} updated successfully`);
+}
+
+function finish(id) {
+    alert(`Item ${id} finished successfully`);
+}
