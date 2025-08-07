1b92a9a1c62617c92d21b0b30bbeae23732130c4
Alexi_Dg <alexisdurangomez588@gmail.com>
2025-08-06 23:42:22 -0500

    feat: :sparkles: cndxbm

--- todoApp/script/script.js
+++ todoApp/script/script.js
@@ -7,22 +7,22 @@ function init() {
         db.forEach((e) => {
             cardContainer.innerHTML += `
             <div class="card w-80 mb-3">
-                    <div class="card-body container">
-                        <div class="row gap-3">
-                            <h5 class="card-title">${e.name}</h5>
-                            <div class="col">
-                                <p class="card-text"><strong>Mensaje</strong> ${e.message}</p>
-                                <p class="card-text"><strong>Categoria:</strong> ${e.category}</p>
-                                <p class="card-text"><strong>Items:</strong> ${e.items.join(", ")}</p>
-                            </div>
-                            <div class="col text-center mb-4">
-                                <button onclick="deleteItem(${e.id})" class="btn btn-danger">eliminar</button>
-                                <button onclick="update(${e.id})" class="btn btn-warning">actualizar</button>
-                                <button onclick="finish(${e.id})" class="btn btn-info">Tachar</button>
-                            </div>
+                <div class="card-body container">
+                    <div class="row gap-3">
+                        <h5 class="card-title">${e.name}</h5>
+                        <div class="col">
+                            <p class="card-text"><strong>Mensaje</strong> ${e.message}</p>
+                            <p class="card-text"><strong>Categoria:</strong> ${e.category}</p>
+                            <p class="card-text"><strong>Items:</strong> ${e.items.join(", ")}</p>
+                        </div>
+                        <div class="col text-center mb-4">
+                            <button onclick="deleteItem(${e.id})" class="btn btn-danger">eliminar</button>
+                            <button onclick="update(${e.id})" class="btn btn-warning">actualizar</button>
+                            <button onclick="finish(${e.id})" class="btn btn-info">Tachar</button>
                         </div>
                     </div>
                 </div>
+            </div>
             `;
         } );
     } else {
@@ -60,8 +60,8 @@ function searchTodo() {
                         </div>
                     </div>
                 </div>
-            </div>
-            `;
+            </div>`
+            ;
     });
 }
 
